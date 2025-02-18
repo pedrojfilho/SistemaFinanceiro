ed19cd69e0814d9cf554dc8c232e40fdd93cfcdc
Pedro Filho <pedrojsilva25@gmail.com>
2025-02-18 15:41:51 -0300

    Retirando arquivos da Pasta Projeto

--- obj/Debug/net9.0/Projeto.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c0db60c66112218954e2c61ad70354aa2d8e75d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
